9a6ee434aba9ecd9583881de81beffb215952ec9
Karen Etheridge <ether@cpan.org>
2013-03-02 17:10:38 -0800

    initial commit, via [Git::Init]

---
+++ lib/Test/Warning.pm
@@ -0,0 +1,40 @@
+package Test::Warning;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 METHODS
+
+=over
+
+=item * C<foo>
+
+=back
+
+...
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<https://rt.cpan.org/Public/Dist/Display.html?Name= >.
+I am also usually active on irc, as 'ether' at L<irc://irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut
