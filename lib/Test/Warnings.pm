eb5076ad341e6bfb2851475bef85cfc306cb56c4
Karen Etheridge <ether@cpan.org>
2020-03-13 22:28:40 -0700

    increment $VERSION after 0.029 release

--- lib/Test/Warnings.pm
+++ lib/Test/Warnings.pm
@@ -5,7 +5,7 @@ package Test::Warnings;
 # ABSTRACT: Test for warnings and the lack of them
 # KEYWORDS: testing tests warnings
 
-our $VERSION = '0.029';
+our $VERSION = '0.030';
 
 use parent 'Exporter';
 use Test::Builder;
